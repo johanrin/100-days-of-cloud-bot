63b7778a4598ff36aaaed63a25a10aece2feea54
Johan Rin <rin.johan@gmail.com>
2020-07-04 17:03:35 +0200

    refactor stream in cursor to prevent error 420

--- bots/favretweet.py
+++ bots/favretweet.py
@@ -4,23 +4,15 @@
 import tweepy
 import logging
 from config import create_api
-import json
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-class FavRetweetListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
-
-    def on_status(self, tweet):
-        logger.info(f"Processing tweet id {tweet.id}")
-        if tweet.in_reply_to_status_id is not None or \
-                tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
+def fav_retweet(api):
+    logger.info("Retrieving, marking as Liked and retweeting")
+    for tweet in tweepy.Cursor(api.search, q="#100DaysOfCloud", lang ="en").items(5):
         if not tweet.favorited:
             # Mark it as Liked, since we have not done it yet
             try:
@@ -33,17 +25,15 @@ class FavRetweetListener(tweepy.StreamListener):
                 tweet.retweet()
             except Exception as e:
                 logger.error("Error on fav and retweet", exc_info=True)
+        time.sleep(5)
 
-    def on_error(self, status):
-        logger.error(status)
-
-
-def main(keywords):
+def main():
     api = create_api()
-    tweets_listener = FavRetweetListener(api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["en"])
+    while True:
+        fav_retweet(api)
+        logger.info("Waiting...")
+        time.sleep(60)
 
 
 if __name__ == "__main__":
-    main(["#100DaysOfCloud"])
+    main()
